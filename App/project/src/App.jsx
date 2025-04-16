0c00cf5cf4558d39e2b9912ec3627e56726832a6
AARYAN  GOLE <72779304+AaryanGole26@users.noreply.github.com>
2025-04-17 00:36:19 +0530

    Update App.jsx

--- App/project/src/App.jsx
+++ App/project/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Hero, Services as LandingServices, Footer } from "./LandingPage";
-import { Header } from "./header.jsx";
+import { Header } from "./Header.jsx";
 import ServicesPage from "./ServicesPage";
 import ServiceChatPage from "./ServiceChatPage";
 import Login from "./Login";
@@ -9,6 +9,8 @@ import Signup from "./Signup";
 import ContactUs from "./ContactUs";
 import AboutUs from "./AboutUs";
 import { supabase } from "./supabase";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -21,27 +23,29 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <Header user={user} />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <LandingServices />
-            </>
-          }
-        />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/service-chat/:serviceTitle" element={<ServiceChatPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/about" element={<AboutUs />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <I18nextProvider i18n={i18n}>
+      <Router>
+        <Header user={user} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <LandingServices />
+              </>
+            }
+          />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/service-chat/:serviceTitle" element={<ServiceChatPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </I18nextProvider>
   );
 }
 
