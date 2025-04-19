84d1849d32da6007a86777cf2d60cdc55a33169f
AARYAN  GOLE <72779304+AaryanGole26@users.noreply.github.com>
2025-04-19 22:54:10 +0530

    Update App.jsx

--- App/project/src/App.jsx
+++ App/project/src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./Login";
 import Signup from "./Signup";
 import ContactUs from "./ContactUs";
 import AboutUs from "./AboutUs";
+import Resources from "./Resources";
 import { supabase } from "./supabase";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
@@ -41,6 +42,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/contact" element={<ContactUs />} />
+          <Route path="/resources" element={<Resources />} />
           <Route path="/about" element={<AboutUs />} />
         </Routes>
         <Footer />
