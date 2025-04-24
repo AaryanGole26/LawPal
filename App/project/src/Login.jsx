ad0a1390aaa177c0dabb714a5a3adcc349c430b7
AARYAN  GOLE <72779304+AaryanGole26@users.noreply.github.com>
2025-04-25 00:32:59 +0530

    Update Login.jsx

--- App/project/src/Login.jsx
+++ App/project/src/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from './supabase';
-
+const apiUrl = import.meta.env.VITE_API_URL;
 function Login() {
   const [authMode, setAuthMode] = useState('password'); // or 'otp'
   const [email, setEmail] = useState('');
